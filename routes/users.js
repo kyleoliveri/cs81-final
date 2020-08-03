81132e7bb4217784d224dda6418169dbbfac8673
Kyle Oliveri <KyleOliveri@Kyles-Mac-Pro-2.local>
2020-08-02 19:42:43 -0700

    Authentication token set up

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/Users');
@@ -12,13 +15,49 @@ router.post('/', [
     check('email', 'Please add a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
 ],
- (req, res) => {
+async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
     
-    res.send('passed');
+    const { name, email, password } = req.body;
+    
+    try {
+        let user = await User.findOne({ email });
+
+        if(user) {
+            return res.status(400).json({ msg: 'User already exists' })
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        //Protects password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
+        
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        //Sends webtoken with an expiration
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000
+        }, (err, token) => {
+            if(err) throw token;
+            res.json({ token });
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
